5d25345b9ee22c688331f3732a5011d379b13c69
unknown <mahmood@gmail.com>
2024-11-26 22:05:11 +0200

    edit webhook

--- index.js
+++ index.js
@@ -1,67 +1,70 @@
-const express = require('express')
-const ExpressMongoSanitize = require('express-mongo-sanitize')
-const xss = require('xss-clean')
-const helmet = require('helmet')
-const cors = require('cors')
-const compression = require('compression')
+const express = require("express");
+const ExpressMongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const helmet = require("helmet");
+const cors = require("cors");
+const compression = require("compression");
+const bodyParser = require("body-parser");
 
-const database = require('./config/database')
-const routesMount = require('./routes')
-const ApiError = require('./utils/apiError')
-const { verifySession } = require('./controllers/user.controller')
+const database = require("./config/database");
+const routesMount = require("./routes");
+const ApiError = require("./utils/apiError");
+const { verifySession } = require("./controllers/user.controller");
 
-const app = express()
+const app = express();
+
+app.use(bodyParser.raw({ type: "application/json" })) // Ensures raw body is available for Stripe);
 
 // Parse Str as Json Middleware
-app.use(express.json())
+app.use(express.json());
 
 // To remove data using Data Sanitize:
 app.use(ExpressMongoSanitize());
-app.use(xss())
+app.use(xss());
 
 // Use Helmet!
 app.use(helmet());
 
 // Enable other domains to access myApp
-app.use(cors({ path: '*' }));
-app.options('*', cors());
+app.use(cors({ path: "*" }));
+app.options("*", cors());
 
 // Compress all response
-app.use(compression())
+app.use(compression());
 
 // Mongoose DB
-database()
+database();
 
 // Webhook
-app.post('/webhook', express.raw({ type: "application/json" }),verifySession)
+app.post("/webhook", verifySession);
 
 // Routes
-routesMount(app)
+routesMount(app);
 
 // Error Handles
-app.all('*', (req, res, next) => {
-    const err = new Error(`Can't find ${req.originalUrl}`)
-    next(new ApiError(err), 400)
-})
+app.all("*", (req, res, next) => {
+    const err = new Error(`Can't find ${req.originalUrl}`);
+    next(new ApiError(err), 400);
+});
 app.use((err, req, res, next) => {
     res.status(400).json(
-        process.env.NODE_ENV === "development" ?
-            {
+        process.env.NODE_ENV === "development"
+            ? {
                 status: err.status || "error",
                 message: err.message,
-                stack: err.stack
-            } :
-            {
+                stack: err.stack,
+            }
+            : {
                 status: err.status || "error",
                 message: err.message,
             }
-    )
-})
+    );
+});
 
 const server = app.listen(3000, () => {
-    console.log("Working....")
-})
+    console.log("Working....");
+});
 
-process.on('unhandledRejection', (err) => {
-    console.log(`Rejection Error: ${err}`)
-})
+process.on("unhandledRejection", (err) => {
+    console.log(`Rejection Error: ${err}`);
+});
